cd1902dc3d462de1081d5c5791d674be7118f000
Kyle Drake <kyledrake@gmail.com>
2012-04-03 11:20:26 -0700

    Delete session[:flash] if it's empty, to avoid sending a cookie and breaking caching schemes

--- lib/sinatra/flash.rb
+++ lib/sinatra/flash.rb
@@ -12,10 +12,12 @@ module Sinatra
 
       # This callback rotates any flash structure we referenced, placing the 'next' hash into the session
       # for the next request.
-      app.after {@flash.each{|key, flash| session[key] = @flash[key].next} if @flash}
+      app.after do
+        @flash.each{|key, flash| session[key] = @flash[key].next} if @flash
+        session.delete :flash if session[:flash] == {}
+      end
     end
-
   end
-  
+
   register Flash
 end
