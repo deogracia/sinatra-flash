ff1e0c438696fe37ef203502eaebbb18ead33ec7
Iain Barnett <iainspeed@gmail.com>
2012-07-02 21:18:05 +0100

    Moved loop used to produce div output to a lambda so that a block can be supplied to the styled_flash method in order to override the default styling. Added specs for it.

--- lib/sinatra/flash/style.rb
+++ lib/sinatra/flash/style.rb
@@ -1,29 +1,47 @@
 module Sinatra
   module Flash
     module Style
+    
+      # This block provides the default styling for the styled_flash method.
+      DEFAULT_BLOCK = ->(id,vals) do
+        %Q!<div id='#{id}'>\n#{vals.collect{|message| "  <div class='flash #{message[0]}'>#{message[1]}</div>\n"}}</div>!
+      end
 
       # A view helper for rendering flash messages to HTML with reasonable CSS structure. Handles 
-      # multiple flash messages in one request. Wraps them in a <div> tag with id #flash containing
-      # a <div> for each message with classes of .flash and the message type.  E.g.:
+      # multiple flash messages in one request.
       #
-      # @example
-      #   <div id='flash'>
-      #     <div class='flash info'>Today is Tuesday, April 27th.</div>
-      #     <div class='flash warning'>Missiles are headed to destroy the Earth!</div>
-      #   </div>
+      # @overload styled_flash(key)
+      #   @param [optional, String, Symbol] key Specifies which flash collection you want to display. 
+      #     If you use this, the collection key will be appended to the top-level div id (e.g.,
+      #     'flash_login' if you pass a key of  :login).
+      #   Wraps them in a <div> tag with id #flash containing
+      #   a <div> for each message with classes of .flash and the message type.  E.g.:
       #
-      # It is your responsibility to style these classes the way you want in your stylesheets.
+      #   @example
+      #     <div id='flash'>
+      #       <div class='flash info'>Today is Tuesday, April 27th.</div>
+      #       <div class='flash warning'>Missiles are headed to destroy the Earth!</div>
+      #     </div>
       #
-      # @param[optional, String, Symbol] key Specifies which flash collection you want to display. 
-      #   If you use this, the collection key will be appended to the top-level div id (e.g.,
-      #   'flash_login' if you pass a key of  :login).  
+      #   It is your responsibility to style these classes the way you want in your stylesheets.
       #
+      # @overload styled_flash(key, &block)
+      #   @param [optional, String, Symbol] key Specifies which flash collection you want to display. 
+      #     If you use this, the collection key will be appended to the top-level html structure's id (e.g.,
+      #     'flash_login' if you pass a key of  :login).
+      #   @yield [id,vals] block 
+      #   @yieldparam [String] id The id to be used for the attribute id of the html output.
+      #   @yieldparam [Hash] vals This param is for the values found in the flash hash by the key given.
+      #
+      #   @example Change the default style from a div to a list.
+      #     my_styling = ->(id,vals){ %Q!<ul id='#{id}'>\n#{vals.collect{|message| "  <li class='flash #{message[0]}'>#{message[1]}</li>\n"}}</ul>! }
+      #     styled_flash(:info, &my_styling)
       # @return [String] Styled HTML if the flash contains messages, or an empty string if it's empty.
-      def styled_flash(key=:flash)
+      def styled_flash(key=:flash, &block)
         return "" if flash(key).empty?
         id = (key == :flash ? "flash" : "flash_#{key}")
-        messages = flash(key).collect {|message| "  <div class='flash #{message[0]}'>#{message[1]}</div>\n"}
-        "<div id='#{id}'>\n" + messages.join + "</div>"
+        block = DEFAULT_BLOCK if block.nil?
+        output = block.call(id,flash(key))
       end
       
     end
--- spec/flash/style_spec.rb
+++ spec/flash/style_spec.rb
@@ -5,39 +5,108 @@ describe 'styled_flash method' do
   include Sinatra::Sessionography  # for the 'session' method
   include Sinatra::Flash::Storage
   include Sinatra::Flash::Style
-  
+
   before(:each) do
-    Sinatra::Sessionography.session = {:flash => {:foo=>:bar, :too=>'tar'},
-                                       :smash => {:yoo=>:yar, :zoo=>'zar'}}
+    Sinatra::Sessionography.session = {
+      :flash => {:foo=>:bar, :too=>'tar'},
+      :smash => {:yoo=>:yar, :zoo=>'zar'}
+    }
   end
-  
+
   it "returns an empty string if the flash is empty" do
     Sinatra::Sessionography.session = {}
     styled_flash.should == ""
   end
-  
+
   it "returns a div of #flash if the structure is the default" do
     styled_flash.should =~ /<div id='flash'>/
   end
-  
+
   it "contains each key as a class" do
     styled_flash.should =~ /<div class='flash foo'>bar<\/div>/
     styled_flash.should =~ /<div class='flash too'>tar<\/div>/
   end
-  
-  describe "if a key is passed" do
-    it "returns an empty string if that structure is empty" do
-      styled_flash(:trash).should == ""
+
+  context "Given a key" do
+    context "When that structure is empty" do
+      it "returns an empty string" do
+        styled_flash(:trash).should == ""
+      end
     end
-    
-    it "returns a div containing the key name if a key is passed" do
-      styled_flash(:smash).should =~ /<div id='flash_smash'>/
+
+    context "When the structure is not empty" do
+      it "returns a div containing the key name" do
+        styled_flash(:smash).should =~ /<div id='flash_smash'>/
+      end
+
+      it "returns each of the keys within that key as a class" do
+        styled_flash(:smash).should =~ /<div class='flash yoo'>yar<\/div>/
+        styled_flash(:smash).should =~ /<div class='flash zoo'>zar<\/div>/
+      end
     end
-    
-    it "returns each of the keys within that key as a class" do
-      styled_flash(:smash).should =~ /<div class='flash yoo'>yar<\/div>/
-      styled_flash(:smash).should =~ /<div class='flash zoo'>zar<\/div>/
-    end
-    
   end
+
+  describe "Block style styling" do
+    context "Given a block" do
+      context " that is the same as the default" do
+        let(:my_block) {
+          ->(id,vals) do
+            %Q!<div id='#{id}'>\n#{vals.collect{|message| "  <div class='flash #{message[0]}'>#{message[1]}</div>\n"}}</div>!
+          end
+        }
+
+        context "When that structure is empty" do
+          it "returns an empty string" do
+            styled_flash(:trash, &my_block).should == ""
+          end
+        end
+
+        context "When the structure is not empty" do
+          it "returns a div containing the key name" do
+            styled_flash(:smash, &my_block).should =~ /<div id='flash_smash'>/
+          end
+
+          it "returns each of the keys within that key as a class" do
+            styled_flash(:smash, &my_block).should =~ /<div class='flash yoo'>yar<\/div>/
+            styled_flash(:smash, &my_block).should =~ /<div class='flash zoo'>zar<\/div>/
+          end
+        end
+      end
+
+      context "That is different to the default" do
+        let(:my_block) {
+          ->(id,vals) do
+            %Q!<ul id='#{id}'>\n#{vals.collect{|message| "  <li class='flash #{message[0]}'>#{message[1]}</li>\n"}}</ul>!
+          end
+        }
+
+        context "When that structure is empty" do
+          it "returns an empty string" do
+            styled_flash(:trash, &my_block).should == ""
+          end
+        end
+
+        context "When the structure is not empty" do
+          it "returns a div containing the key name" do
+            styled_flash(:smash, &my_block).should =~ /<ul id='flash_smash'>/
+          end
+
+          it "returns each of the keys within that key as a class" do
+            styled_flash(:smash, &my_block).should =~ /<li class='flash yoo'>yar<\/li>/
+            styled_flash(:smash, &my_block).should =~ /<li class='flash zoo'>zar<\/li>/
+          end
+          context "Given a block in the standard style" do
+            subject {
+              styled_flash :smash do |id,vals|
+                %Q!<ul id='#{id}'>\n#{vals.collect{|message| "  <li class='flash #{message[0]}'>#{message[1]}</li>\n"}}</ul>!
+              end
+            }
+            it { should =~ /<li class='flash zoo'>zar<\/li>/ }
+          end
+        end
+
+      end
+    end
+  end
+
 end
